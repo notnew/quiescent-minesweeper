8bc6e559e53f9fdf131aae829bb7aea48e0692e0
notnew <you@example.com>
2015-09-12 18:57:30 -0500

    add components to draw tiles

--- src/minesweeper/views.cljs
+++ src/minesweeper/views.cljs
@@ -3,9 +3,26 @@
               [quiescent.dom :as dom]
               [minesweeper.dispatch :as dispatch]))
 
+(defcomponent Tile
+  :keyfn (fn [{:keys [x y]}]
+           [x y])
+
+  [{:keys [x y]}]
+  (dom/rect {:x (* x 10)
+             :y (* y 10)
+             :width 9 :height 9
+             :fill "grey"}))
+
+
+(defcomponent Board
+  [board]
+  (apply dom/svg {}
+         (map Tile (flatten board))))
+
 (defcomponent Main-panel
   [state]
   (dom/div {}
      "Hello from minesweeper project"
+     (Board (:board state))
      (dom/p)
      "Your state is: " (pr-str state)))
