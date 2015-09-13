6b6c87a8b753bd610f5fe4c6626f173aa7539f9c
notnew <you@example.com>
2015-09-13 00:05:13 -0500

    check bombs in `clear-tile!` function
    call dummy `detonate!` function if clicked tile has bomb
    otherwise count number of bombs in neighboring tiles

--- src/minesweeper/dispatch.cljs
+++ src/minesweeper/dispatch.cljs
@@ -15,9 +15,18 @@
     (swap! state assoc :board board
                        :mode :playing)))
 
+(defn detonate!
+  [tile]
+  (js/alert "Boom!"))
+
 (defn clear-tile!
   "return an event handler that clears the provided tile when the event fires"
-  [{:keys [x y]}]
+  [{:keys [x y] :as tile}]
   (fn [_]
-    (swap! state assoc-in [:board :tiles y x :cleared?] 0)))
+    (if (:bomb? tile)
+        (detonate! tile)
+        (let [neighbors (minesweeper.board/get-neighbors tile (:board @state))
+              bomb-count (count (keep :bomb? neighbors))]
+          (swap! state assoc-in [:board :tiles y x :cleared?]
+                 bomb-count)))))
 
