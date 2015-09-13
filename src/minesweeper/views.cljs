c5cb4992ddd70423f323d085e9855a05bde41e9a
notnew <you@example.com>
2015-09-12 19:32:54 -0500

    scale tiles; add background

--- src/minesweeper/views.cljs
+++ src/minesweeper/views.cljs
@@ -3,20 +3,31 @@
               [quiescent.dom :as dom]
               [minesweeper.dispatch :as dispatch]))
 
+(def tile-width 100)
+
 (defcomponent Tile
   :keyfn (fn [{:keys [x y]}]
            [x y])
 
   [{:keys [x y]}]
-  (dom/rect {:x (* x 10)
-             :y (* y 10)
-             :width 9 :height 9
+  (dom/rect {:x (* x tile-width)
+             :y (* y tile-width)
+             :width (* 0.9 tile-width)
+             :height (* 0.9 tile-width)
              :fill "grey"}))
 
+(defcomponent Background
+  []
+  (dom/rect {:width "100%" :height "100%" :fill "lightgrey"}))
+
 (defcomponent Board
-  [board]
-  (apply dom/svg {}
-         (map Tile (flatten (:tiles board)))))
+  [{:keys [tiles width height]}]
+  (dom/svg {:width 400 :height 400
+            :viewBox (str "0 0 " (* width tile-width) " " (* height tile-width))
+            }
+   (Background)
+   (apply dom/g {}
+           (map Tile (flatten tiles)))))
 
 (defcomponent Main-panel
   [state]
