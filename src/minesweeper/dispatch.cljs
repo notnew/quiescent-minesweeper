dc7463830298286fbbb95f2de12094488a6b43e8
notnew <you@example.com>
2015-09-13 12:14:11 -0500

    refactor; move pure code from `dispatch` to `state`

--- src/minesweeper/dispatch.cljs
+++ src/minesweeper/dispatch.cljs
@@ -1,60 +1,15 @@
 (ns minesweeper.dispatch
-    (:require [minesweeper.state :refer [state]]
-              [minesweeper.board
-               :refer [make-board set-tile get-neighbors map-tiles update-tile]]
-              ))
+    (:require [minesweeper.state :refer [state new-game clear-tile]]
+              [minesweeper.board :refer [set-tile update-tile]]))
 
-(declare mark-neighbor-counts)
-
-(defn init-board!
-  []
-  (let [fresh-board (make-board 9 9)
-        bomb-positions [{:x 3, :y 3} {:x 7, :y 3}
-                        {:x 1, :y 1} {:x 6, :y 4}
-                        {:x 5, :y 7} {:x 0, :y 0}]
-        add-bomb (fn [board position]
-                   (set-tile board position :bomb? true))
-        board (reduce add-bomb fresh-board bomb-positions)
-        board (mark-neighbor-counts board)]
-    (swap! state assoc :board board
-                       :mode :playing)))
+(defn init-board! []
+  (swap! state new-game))
 
 (defn detonate!
   [tile]
   (swap! state update-in [:board]
          set-tile tile :detonated? true)
   (swap! state assoc :mode :dead))
-
-(defn count-neighboring
-  [board tile]
-  (let [neighbors (get-neighbors board tile)]
-    (count (keep :bomb? neighbors))))
-
-(defn mark-neighbor-counts
-  [board]
-  (let [mark-neighbor-count (fn [tile]
-                              (assoc tile :neighboring-bombs
-                                     (count-neighboring board tile)))]
-    (map-tiles board mark-neighbor-count)))
-
-(defn clear-tile
-  [board tile]
-  (if (:cleared? tile)
-    board
-    (let [flood-safe  ; flood-fill to find `safe` tiles to clear
-          (fn flood-safe [acc tile]
-            (cond
-             (acc tile) acc
-
-             (= 0 (:neighboring-bombs tile)) ; safe: apply to all neighbors
-             (reduce flood-safe (merge acc tile)
-                                (get-neighbors board tile))
-
-             :else (merge acc tile)))
-
-          clear (fn [board tile]
-                  (set-tile board tile :cleared? true))]
-      (reduce clear board (flood-safe #{} tile)))))
 
 (defn clear-tile!
   "return an event handler that clears the provided tile when the event fires"
--- src/minesweeper/state.cljs
+++ src/minesweeper/state.cljs
@@ -1,7 +1,53 @@
-(ns minesweeper.state)
+(ns minesweeper.state
+  (:require
+   [minesweeper.board :refer [make-board get-neighbors
+                              set-tile update-tile map-tiles]]))
 
 (defonce state (atom {:mode :start
                       :bomb-count 10
                       :board {:width 9 :height 9}}))
 
+(defn- count-neighboring
+  [board tile]
+  (let [neighbors (get-neighbors board tile)]
+    (count (keep :bomb? neighbors))))
 
+(defn- mark-neighbor-counts
+  [board]
+  (let [mark-neighbor-count (fn [tile]
+                              (assoc tile :neighboring-bombs
+                                     (count-neighboring board tile)))]
+    (map-tiles board mark-neighbor-count)))
+
+(defn new-game
+  [state]
+  (let [{:keys [width height]} (:board state)
+        fresh-board (make-board width height)
+        bomb-positions [{:x 3, :y 3} {:x 7, :y 3}
+                        {:x 1, :y 1} {:x 6, :y 4}
+                        {:x 5, :y 7} {:x 0, :y 0}]
+        add-bomb (fn [board position]
+                   (set-tile board position :bomb? true))
+        board (reduce add-bomb fresh-board bomb-positions)
+        board (mark-neighbor-counts board)
+        ]
+    (assoc state :board board :mode :playing)))
+
+(defn clear-tile
+  [board tile]
+  (if (:cleared? tile)
+    board
+    (let [flood-safe  ; flood-fill to find `safe` tiles to clear
+          (fn flood-safe [acc tile]
+            (cond
+             (acc tile) acc
+
+             (= 0 (:neighboring-bombs tile)) ; safe: apply to all neighbors
+             (reduce flood-safe (merge acc tile)
+                                (get-neighbors board tile))
+
+             :else (merge acc tile)))
+
+          clear (fn [board tile]
+                  (set-tile board tile :cleared? true))]
+      (reduce clear board (flood-safe #{} tile)))))
