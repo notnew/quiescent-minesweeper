eb9bbba4b32c4d3e2df1d53ecf2d53bbb74297ea
notnew <you@example.com>
2015-09-13 11:05:07 -0500

    automatically clear safe neighbors
     when a cleared-tile has 0 neighbors with a bomb, do a flood fill to
     clear the entire safe area

--- src/minesweeper/dispatch.cljs
+++ src/minesweeper/dispatch.cljs
@@ -1,18 +1,19 @@
 (ns minesweeper.dispatch
     (:require [minesweeper.state :refer [state]]
-              [minesweeper.board]
+              [minesweeper.board
+               :refer [make-board set-tile get-neighbors map-tiles]]
               ))
 
 (declare mark-neighbor-counts)
 
 (defn init-board!
   []
-  (let [fresh-board (minesweeper.board/make-board 9 9)
+  (let [fresh-board (make-board 9 9)
         bomb-positions [{:x 3, :y 3} {:x 7, :y 3}
                         {:x 1, :y 1} {:x 6, :y 4}
                         {:x 5, :y 7} {:x 0, :y 0}]
         add-bomb (fn [board position]
-                   (minesweeper.board/set-tile board position :bomb? true))
+                   (set-tile board position :bomb? true))
         board (reduce add-bomb fresh-board bomb-positions)
         board (mark-neighbor-counts board)]
     (swap! state assoc :board board
@@ -21,27 +22,39 @@
 (defn detonate!
   [tile]
   (swap! state update-in [:board]
-         minesweeper.board/set-tile tile :detonated? true)
+         set-tile tile :detonated? true)
   (swap! state assoc :mode :dead))
 
 (defn count-neighboring
   [board tile]
-  (let [neighbors (minesweeper.board/get-neighbors board tile)]
+  (let [neighbors (get-neighbors board tile)]
     (count (keep :bomb? neighbors))))
 
 (defn mark-neighbor-counts
   [board]
   (let [mark-neighbor-count (fn [tile]
                               (assoc tile :neighboring-bombs
-                                          (count-neighboring board tile)))
-        map-tiles minesweeper.board/map-tiles]
+                                     (count-neighboring board tile)))]
     (map-tiles board mark-neighbor-count)))
 
 (defn clear-tile
   [board tile]
   (if (:cleared? tile)
     board
-    (minesweeper.board/set-tile board tile :cleared? true)))
+    (let [flood-safe  ; flood-fill to find `safe` tiles to clear
+          (fn flood-safe [acc tile]
+            (cond
+             (acc tile) acc
+
+             (= 0 (:neighboring-bombs tile)) ; safe: apply to all neighbors
+             (reduce flood-safe (merge acc tile)
+                                (get-neighbors board tile))
+
+             :else (merge acc tile)))
+
+          clear (fn [board tile]
+                  (set-tile board tile :cleared? true))]
+      (reduce clear board (flood-safe #{} tile)))))
 
 (defn clear-tile!
   "return an event handler that clears the provided tile when the event fires"
