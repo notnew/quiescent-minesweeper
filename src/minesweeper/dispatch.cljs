4b8b18276bc536de581678bef4cbbca98a0a9227
notnew <you@example.com>
2015-09-12 20:04:06 -0500

    implement clearing of tiles in views' components
    also nicer border/background of tiles

--- src/minesweeper/dispatch.cljs
+++ src/minesweeper/dispatch.cljs
@@ -7,3 +7,10 @@
   []
   (let [new-board (minesweeper.board/make-board 9 9)]
     (swap! state assoc :board new-board)))
+
+(defn clear-tile!
+  "return an event handler that clears the provided tile when the event fires"
+  [{:keys [x y]}]
+  (fn [_]
+    (swap! state assoc-in [:board :tiles y x :cleared?] 0)))
+
--- src/minesweeper/views.cljs
+++ src/minesweeper/views.cljs
@@ -9,23 +9,31 @@
   :keyfn (fn [{:keys [x y]}]
            [x y])
 
-  [{:keys [x y]}]
-  (dom/rect {:x (* x tile-width)
-             :y (* y tile-width)
-             :width (* 0.9 tile-width)
-             :height (* 0.9 tile-width)
-             :fill "grey"}))
-
-(defcomponent Background
-  []
-  (dom/rect {:width "100%" :height "100%" :fill "lightgrey"}))
+  [{:keys [x y] :as tile}]
+  (dom/g
+   {}
+   (dom/rect {:x (* x tile-width)
+              :y (* y tile-width)
+              :width tile-width
+              :height tile-width
+              :fill (if (:cleared? tile) "lightgrey" "grey")
+              :stroke "darkgrey"
+              :strokeWidth (* 0.05 tile-width)
+              :onClick (dispatch/clear-tile! tile)
+              }
+             )
+   (dom/text {:x (* (+ 0.25 x) tile-width)
+              :y (* (+ 0.8 y) tile-width)
+              :fontSize (* 0.8 tile-width) }
+             (when-let [count (:cleared? tile)]
+               (when (not= count 0)
+                 count)))))
 
 (defcomponent Board
   [{:keys [tiles width height]}]
   (dom/svg {:width 400 :height 400
             :viewBox (str "0 0 " (* width tile-width) " " (* height tile-width))
             }
-   (Background)
    (apply dom/g {}
            (map Tile (flatten tiles)))))
 
