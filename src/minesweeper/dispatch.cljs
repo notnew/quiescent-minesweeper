ef0cd7c0e0ed1c70f8f28ec21706f77b25641819
notnew <you@example.com>
2015-09-12 23:50:32 -0500

    add some (non-randomly) bombs to board on init

--- src/minesweeper/dispatch.cljs
+++ src/minesweeper/dispatch.cljs
@@ -5,8 +5,14 @@
 
 (defn init-board!
   []
-  (let [new-board (minesweeper.board/make-board 9 9)]
-    (swap! state assoc :board new-board
+  (let [fresh-board (minesweeper.board/make-board 9 9)
+        bomb-positions [{:x 3, :y 3} {:x 7, :y 3}
+                        {:x 1, :y 1} {:x 6, :y 4}
+                        {:x 5, :y 7} {:x 0, :y 0}]
+        add-bomb (fn [board {:keys [x y]}]
+                   (assoc-in board [:tiles y x :bomb?] true))
+        board (reduce add-bomb fresh-board bomb-positions)]
+    (swap! state assoc :board board
                        :mode :playing)))
 
 (defn clear-tile!
