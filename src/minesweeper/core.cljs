cdfb448820ecf30d48d77f72051c054503ed6625
notnew <you@example.com>
2015-09-15 12:31:14 -0500

    use requestAnimationFrame for rendering

--- src/minesweeper/core.cljs
+++ src/minesweeper/core.cljs
@@ -6,9 +6,30 @@
 
 (def app-mount-point (.getElementById js/document "app"))
 
+(defonce dirty? (atom false))
+
 (defn render! []
+  (reset! dirty? false)
   (quiescent/render (views/Main-panel @state/state) app-mount-point))
+
+(add-watch state/state :set-dirty
+           (fn [_ _ old new]
+             (when (and (not @dirty?)
+                        (not= old new))
+               (reset! dirty? true))))
+
+(def requestAnimationFrame
+  (or (.. js/window -requestAnimationFrame)
+      (.. js/window -webkitRequestAnimationFrame)
+      (.. js/window -mozRequestAnimationFrame)
+      (.. js/window -msRequestAnimationFrame)
+      (fn [raf-callback]
+        (js/setTimeout raf-callback 16))))
+
+(add-watch dirty? :render-when-dirty
+           (fn [_ _ old new]
+             (when (and new (not old))  ; render if dirty? was just set
+               (requestAnimationFrame render!))))
 
 (render!)
 
-(add-watch state/state :render-on-change (fn [_ _ _ _] (render!)))
